b3bf529ae3c02c895659df7223c3cb74c1801cea
Alexander Six <alexanderhsix@gmail.com>
2024-01-11 21:29:58 -0500

    Initial commit

---
+++ resources/views/components.blade.php
@@ -0,0 +1,12 @@
+<x-layouts.app>
+    <div class="flex flex-col justify-center items-center p-8 min-h-screen">
+        <x-filament::section>
+            <x-slot name="heading">
+                This is a section
+            </x-slot>
+        </x-filament::section>
+        <x-filament::button size="lg" icon="heroicon-m-sparkles">
+            This is a button
+        </x-filament::button>
+    </div>
+</x-layouts.app>
